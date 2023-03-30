235155ffddfdbf13c9fbb1b7728c1ac27eb9f1ca
BenG123 <61527372+BenGardiner123@users.noreply.github.com>
2023-03-30 20:06:05 +1000

    Add project files.

---
+++ VirtualCoffeeMachine/Program.cs
@@ -0,0 +1,121 @@
+﻿// See https://aka.ms/new-console-template for more information
+using VirtualCoffeeMachine;
+CoffeeMachine coffeeMachine = new CoffeeMachine();
+
+
+while (true)
+{
+    Console.WriteLine("Current balance: {0:C}", coffeeMachine.Balance);
+
+    Console.WriteLine("Enter 'A' to add coins");
+
+    if (coffeeMachine.Balance >= 3.50M)
+    {
+        Console.WriteLine("Enter 1 to buy Cappuccino");
+    }
+
+    if (coffeeMachine.Balance >= 3.00M)
+    {
+        Console.WriteLine("Enter 2 to buy Latte");
+    }
+
+    if (coffeeMachine.Balance >= 4.00M)
+    {
+        Console.WriteLine("Enter 3 to buy Decaf");
+    }
+
+    Console.WriteLine("Enter 0 to exit");
+
+    string choice = Console.ReadLine().ToLower();
+
+    switch (choice)
+    {
+        case "0":
+            if (coffeeMachine.Balance > 0M)
+            {
+                Dictionary<decimal, int> change = coffeeMachine.ReturnChange();
+                Console.WriteLine("Dispensing change:");
+                foreach (KeyValuePair<decimal, int> kvp in change)
+                {
+                    Console.WriteLine("{0:C} x {1}", kvp.Key, kvp.Value);
+                }
+            }
+            Environment.Exit(0);
+            break;
+        case "1":
+            if (coffeeMachine.Price <= coffeeMachine.Balance)
+            {
+                if (coffeeMachine.SelectCoffee())
+                {
+                    Console.WriteLine("Enjoy your Cappuccino!");
+                }
+                else
+                {
+                    Console.WriteLine("Unable to dispense Cappuccino.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance to buy Cappuccino.");
+            }
+            break;
+        case "2":
+            if (coffeeMachine.Price <= coffeeMachine.Balance)
+            {
+                if (coffeeMachine.SelectCoffee())
+                {
+                    Console.WriteLine("Enjoy your Latte!");
+                }
+                else
+                {
+                    Console.WriteLine("Unable to dispense Latte.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance to buy Latte.");
+            }
+            break;
+        case "3":
+            if (coffeeMachine.Price <= coffeeMachine.Balance)
+            {
+                if (coffeeMachine.SelectCoffee())
+                {
+                    Console.WriteLine("Enjoy your Decaf!");
+                }
+                else
+                {
+                    Console.WriteLine("Unable to dispense Decaf.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance to buy Decaf.");
+            }
+            break;
+        case "a":
+            Console.WriteLine("Enter coin value (0.1, 0.2, 0.5, 1, 2):");
+            decimal coinValue = decimal.Parse(Console.ReadLine());
+            if (coffeeMachine.InsertCoin(coinValue))
+            {
+                Console.WriteLine("Coin accepted.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid coin.");
+            }
+            break;
+        default:
+            Console.WriteLine("Invalid choice.");
+            break;
+    }
+
+    Console.WriteLine();
+}
+
+
+
+
+
+
+
---
+++ VirtualCoffeeMachine/VirtualCoffeeMachine.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualCoffeeMachine
+{
+    public class CoffeeMachine
+    {
+        public decimal Price { get; set; } = 2.50M;
+        public decimal Balance { get; private set; } = 0M;
+        public decimal TotalSales { get; private set; } = 0M;
+        public int CupsOfCoffee { get; private set; } = 10;
+        public Dictionary<decimal, int> ChangeAvailable { get; private set; } = new Dictionary<decimal, int>()
+    {
+        { 2M, 10 },
+        { 1M, 10 },
+        { 0.5M, 10 },
+        { 0.2M, 10 },
+        { 0.1M, 10 }
+    };
+
+        public bool InsertCoin(decimal coin)
+        {
+            if (coin == 2M || coin == 1M || coin == 0.5M || coin == 0.2M || coin == 0.1M)
+            {
+                Balance += coin;
+                return true;
+            }
+            return false;
+        }
+
+        public bool SelectCoffee()
+        {
+            if (Balance >= Price && CupsOfCoffee > 0)
+            {
+                // Dispense coffee
+                Balance -= Price;
+                TotalSales += Price;
+                CupsOfCoffee--;
+                return true;
+            }
+            return false;
+        }
+
+        public Dictionary<decimal, int> ReturnChange()
+        {
+            Dictionary<decimal, int> change = new Dictionary<decimal, int>();
+            decimal amountToReturn = Balance;
+
+            foreach (KeyValuePair<decimal, int> kvp in ChangeAvailable.OrderByDescending(kvp => kvp.Key))
+            {
+                int coinsToReturn = (int)Math.Floor(amountToReturn / kvp.Key);
+
+                if (coinsToReturn > kvp.Value)
+                {
+                    coinsToReturn = kvp.Value;
+                }
+
+                if (coinsToReturn > 0)
+                {
+                    change.Add(kvp.Key, coinsToReturn);
+                    amountToReturn -= kvp.Key * coinsToReturn;
+                    ChangeAvailable[kvp.Key] -= coinsToReturn;
+                }
+
+                if (amountToReturn == 0)
+                {
+                    Balance = 0M;
+                    return change;
+                }
+            }
+
+            return null;
+        }
+    
+
+
+
+}
+}
